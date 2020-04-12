421fd48ca5d9357f974854e6fc748facdd74b580
Joao Araujo <jgv.araujo@gmail.com>
2020-04-12 18:29:53 -0300

    GCP installation and commands, screenshots of Google Cloud Build's configuration and .dockerignore file

--- app_files/app.py
+++ app_files/app.py
@@ -33,4 +33,4 @@ def predictor():
     return jsonify(status='ok', predict=pred)
 
 if __name__=='__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 8080))
+    app.run( debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)) )
---
+++ gcp_commands/install_gcloud_sdk.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Content from `https://cloud.google.com/sdk/docs/downloads-apt-get`
+
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get install apt-transport-https ca-certificates gnupg curl
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install google-cloud-sdk
